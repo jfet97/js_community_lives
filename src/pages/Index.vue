fde184358ba9de773444e5c0136cd2ea4e0465c9
Andrea Simone Costa <andrysimo1997@gmail.com>
2020-04-01 21:28:44 +0200

    query allGoogleSheet data

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -13,11 +13,26 @@
         <b>gratuiti</b> sottoforma di live, nelle quali vengono trattati i temi più caldi del momento: JavaScript, TypeScript, React, Angular, RxJS e molto altro!
       </p>
     </div>
-    <div class="mt-20 w-full">
+    <div class="mt-16 w-full">
       <g-image src="~/assets/images/community.jpg" height="700" fit="cover" />
     </div>
   </MainPageLayout>
 </template>
+
+<page-query>
+  query {
+    allGoogleSheet {
+      edges {
+        node {
+          id
+          title
+          description
+          speakers
+        }
+      }
+    }
+  }
+</page-query>
 
 <script>
 export default {
@@ -26,6 +41,17 @@ export default {
     charset: "utf-8",
     name: "viewport",
     content: "width=device-width, initial-scale=1"
+  },
+  computed: {
+    lives() {
+      // [{ id, description, title, speakers }]
+      return this.$page.allGoogleSheet.edges
+        .map(edge => edge.node)
+        .map(live => ({
+          ...live,
+          speakers: live.speakers.replace(/\n/g, "").split(",")
+        }));
+    }
   }
 };
 </script>
