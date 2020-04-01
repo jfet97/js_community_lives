34dc64cefeca5656e38ad7de366e1562c5c54794
Andrea Simone Costa <andrysimo1997@gmail.com>
2020-04-01 20:15:53 +0200

    add icons to layout that point to fb groups

--- gridsome.config.js
+++ gridsome.config.js
@@ -16,5 +16,12 @@ module.exports = {
   },
   {
     use: 'gridsome-plugin-tailwindcss',
-  }]
+  }],
+  chainWebpack: config => {
+    const svgRule = config.module.rule('svg')
+    svgRule.uses.clear()
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+  }
 }
--- src/layouts/MainPageLayout.vue
+++ src/layouts/MainPageLayout.vue
@@ -4,9 +4,36 @@
       <div class="w-screen flex justify-center">
         <div class="flex flex-col">
           <h1
-            class="text-4xl text-teal-500 font-montserrat font-black mt-6 upper"
-          >JavaScript Italia Community Lives</h1>
-          <div>Icons</div>
+            class="text-4xl text-teal-500 font-montserrat font-black mt-12 capitalize"
+          >JavaScript developer italiani - Lives</h1>
+          <div class="flex justify-center mt-4">
+            <a
+              href="https://www.facebook.com/groups/javascript.developer.italiani/"
+              target="_blank"
+            >
+              <JavaScriptIcon
+                class="fill-current text-teal-900 hover:text-yellow-500 inline-block h-6 w-6 mr-2 transition duration-500 ease-in-out"
+              />
+            </a>
+            <a href="https://www.facebook.com/groups/angularjs.developer.italiani/" target="_blank">
+              <AngularIcon
+                class="fill-current text-teal-900 hover:text-red-500 inline-block h-6 w-6 mx-2 transition duration-500 ease-in-out"
+              />
+            </a>
+            <a href="https://www.facebook.com/groups/react.developer.italiani/" target="_blank">
+              <ReactIcon
+                class="fill-current text-teal-900 hover:text-blue-500 inline-block h-6 w-6 mx-2 transition duration-500 ease-in-out"
+              />
+            </a>
+            <a
+              href="https://www.facebook.com/groups/opportunita.developer.italiani/"
+              target="_blank"
+            >
+              <OPDIIcon
+                class="fill-current text-teal-900 hover:text-gray-500 inline-block h-6 w-6 ml-2 transition duration-500 ease-in-out"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </header>
@@ -21,6 +48,23 @@ query {
   }
 }
 </static-query>
+
+<script>
+import JavaScriptIcon from "~/assets/svgs/javascript.svg";
+import AngularIcon from "~/assets/svgs/angular.svg";
+import ReactIcon from "~/assets/svgs/react.svg";
+import OPDIIcon from "~/assets/svgs/buddy.svg";
+
+export default {
+  components: {
+    JavaScriptIcon,
+    AngularIcon,
+    ReactIcon,
+    OPDIIcon
+  }
+};
+</script>
+
 
 
 <style>
