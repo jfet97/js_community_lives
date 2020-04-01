46177355f9bee67b87c9517c5accb6aff94674cc
Andrea Simone Costa <andrysimo1997@gmail.com>
2020-04-01 18:00:35 +0200

    add fonts

--- src/main.js
+++ src/main.js
@@ -6,4 +6,9 @@ import MainPageLayout from '~/layouts/MainPageLayout.vue'
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.component('MainPageLayout', MainPageLayout)
+
+  head.link.push({
+    rel: 'stylesheet',
+    href: `https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,400;0,700;1,400&family=Montserrat:ital,wght@0,700;1,400;1,900&display=swap`
+  })
 }
